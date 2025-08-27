1bba2027396d98ca065d10dcac8d956a49fbe875
unknown <mashapoguajay@gmail.com>
2025-08-27 13:28:25 +0530

    check for updates

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-   <h1>Test for deployment</h1>
+    <h1>Test for deployment</h1>
+    <h2>Test for deployment</h2>
 
     </>
   )
